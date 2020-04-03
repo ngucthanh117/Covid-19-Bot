a9ceec966dd8e67554098fe31764a4ed970a8296
ngucthanh117 <42341973+ngucthanh117@users.noreply.github.com>
2020-04-03 16:15:48 +0700

    Update v1.0
    Update v1.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {Client, MessageEmbed ,MessageAttachment} = require('discord.js');
 
 const bot = new Client();
 
-const token = process.env.token;
+//const token = Njc2MzkwNTQ5NDM5MTE5MzYx.Xob8ww.s7rzPsw8BdK6qF1_2z1q2MNc3A8;
 
 const PREFIX = '*';
 
@@ -50,4 +50,4 @@ bot.on('message', async message => {
            break;
    }
 })
-bot.login(token);     
+bot.login(process.env.BOT_TOKEN);     
